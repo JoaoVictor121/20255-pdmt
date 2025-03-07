8c6c724bc268fe4ba17fa54074aed79fd1111bad
Joao Victor <joao28victor22@gmail.com>
2025-03-07 16:25:17 -0300

    learn(ap03-react): encerra o codigo 2.7.2

---
+++ estacao-climatica-classes/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icona: null
+    }
+  }
+}
+render(){
+  return( 
+    <div>Meu App</div>
+  )
+}
+export default App
